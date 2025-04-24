5c3fee0c3353e60224d522b26059d24f7bb30a12
Shu Takahashi <7193372+shutallbridge@users.noreply.github.com>
2025-04-25 00:09:17 +1000

    feat: home page

---
+++ src/components/featured-project-card/index.ts
@@ -0,0 +1,5 @@
+import FeaturedProjectCard, {
+  type Props as FeaturedProjectCardProps,
+} from "./featured-project-card.astro";
+
+export { type FeaturedProjectCardProps, FeaturedProjectCard };
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,18 +1,23 @@
 import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
 
-const projectSchema = z.object({
+const projectSchemaWithoutCover = z.object({
   title: z.string(),
   summary: z.string(),
   duration: z.string(),
   links: z.array(z.object({ label: z.string(), url: z.string().url() })),
+  featuredOrder: z.number().optional(),
 });
 
 const projects = defineCollection({
   loader: glob({ pattern: "**/*.mdx", base: "./src/projects" }),
-  schema: projectSchema,
+  schema: ({ image }) =>
+    projectSchemaWithoutCover.extend({
+      cover: image(),
+      coverAlt: z.string(),
+    }),
 });
 
 const collections = { projects };
 
-export { projectSchema, collections };
+export { projectSchemaWithoutCover, collections };

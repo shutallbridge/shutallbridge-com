6d1c3328465cee0cd7d522b3dc352a15ffbddc69
Shu Takahashi <7193372+shutallbridge@users.noreply.github.com>
2025-04-25 23:24:01 +1000

    feat: image display

---
+++ src/components/full-bleed/index.ts
@@ -0,0 +1,3 @@
+import FullBleed, { type Props as FullBleedProps } from "./full-bleed.astro";
+
+export { type FullBleedProps, FullBleed };
---
+++ src/components/image-display/index.ts
@@ -0,0 +1,5 @@
+import ImageDisplay, {
+  type Props as ImageDisplayProps,
+} from "./image-display.astro";
+
+export { type ImageDisplayProps, ImageDisplay };

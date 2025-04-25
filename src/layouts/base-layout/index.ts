4b9af2e2d7291aa0b0221326b8a5d7e747dde5d2
Shu Takahashi <7193372+shutallbridge@users.noreply.github.com>
2025-04-25 15:31:06 +1000

    feat: about page

--- src/layouts/base-layout/index.ts
+++ src/layouts/base-layout/index.ts
@@ -1,3 +1,6 @@
-import BaseLayout from "./base-layout.astro";
+import BaseLayout, {
+  type Props as BaseLayoutProps,
+  type Styles as BaseLayoutStyles,
+} from "./base-layout.astro";
 
-export { BaseLayout };
+export { type BaseLayoutProps, type BaseLayoutStyles, BaseLayout };

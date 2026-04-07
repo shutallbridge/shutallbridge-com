dd3bee068bc38f1f2aee739cc77aeecf40d0bf0b
Shu Takahashi <7193372+shutallbridge@users.noreply.github.com>
2026-04-07 13:51:21 +0900

    chore: upgrade to astro v6

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,14 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwindcss from "@tailwindcss/vite";
 
-import icon from "astro-icon";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), icon()],
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  integrations: [mdx()],
   devToolbar: {
     enabled: false,
+  },
+  vite: {
+    // @ts-expect-error
+    plugins: [tailwindcss()],
   },
 });
